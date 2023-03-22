73d1c787f1779204b8fca477f7d9ea37e7e9f106
robertvlad <vladrob96@gmail.com>
2023-03-22 04:18:13 +0100

    Bottone genera collegato a javascript

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let genera = document.getElementById('genera');
+
+genera.addEventListener('click', function() {
+
+    let name_surname = document.getElementById('name_surname').value;
+    let km = document.getElementById('km').value;
+    let age = document.getElementById('age').value;
+
+    console.log(name_surname);
+    console.log(age);
+    console.log(km);
+});
+
